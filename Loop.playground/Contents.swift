b025d0c9ce2d9999c3c2833675316c87ed5d1ce5
masterUNG <phrombutr@gmail.com>
2019-01-24 11:25:12 +0700

    for and while Success

---
+++ Loop.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var numbers = [1,2,3,4,5]
+
+for myNumber in numbers {
+    print("myNumber ==> \(myNumber)")
+}
+
+var count: Int = 0
+while count < 10 {
+    print("count ==> \(count)")
+    count += 1
+}
+
+

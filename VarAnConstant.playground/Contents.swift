13bfbb7c57ce7850f55eb7ecffc6b57dc44d75fa
masterUNG <phrombutr@gmail.com>
2019-01-24 09:41:02 +0700

    Var and let

---
+++ VarAnConstant.playground/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+//Variable and Constant
+
+//Implicit Variable
+var nameString = "MasterUNG"
+var numberInt = 100
+var numberDou = 12.34
+var statusBol = true
+
+//Explicit Variable
+var surname: String = "Prombutr"
+var number2: Int = 500
+var number3: Double = 3.1416
+var status2: Bool = false
+
+//Constant
+let myName: String = "Doramon"
+
+
+nameString = "MasterUNG1234"
+//myName = "Doramon1234" Cannot Compile Error
+
+
+
+
+
+
+
+
+
